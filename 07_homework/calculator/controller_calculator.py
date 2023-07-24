1779d23259bf4191cb10809de1c8e31291347c70
german25 <german.apx@yandex.ru>
2023-07-24 10:12:13 +0300

    Homework #07 (+if-else in calc controller)

--- 07_homework/calculator/controller_calculator.py
+++ 07_homework/calculator/controller_calculator.py
@@ -40,22 +40,39 @@ class ControllerCalculator:
 
         while True:
             cmd: str = input('Введите команду: ').strip()
-            match cmd:
-                case '+':
-                    self.calc.addition(self.get_arg('Введите 2-й аргумент: '))
-                case '-':
-                    self.calc.subtraction(
-                        self.get_arg('Введите 2-й аргумент: '))
-                case '*':
-                    self.calc.multiplication(
-                        self.get_arg('Введите 2-й аргумент: '))
-                case '/':
-                    self.calc.division(self.get_arg('Введите 2-й аргумент: '))
-                case '=':
-                    print(self.calc.get_result())
-                    self.ask_user()
-                case _:
-                    print('Неизвестная команда')
+            # # конструкция match - case
+            # match cmd:
+            #     case '+':
+            #         self.calc.addition(self.get_arg('Введите 2-й аргумент: '))
+            #     case '-':
+            #         self.calc.subtraction(
+            #             self.get_arg('Введите 2-й аргумент: '))
+            #     case '*':
+            #         self.calc.multiplication(
+            #             self.get_arg('Введите 2-й аргумент: '))
+            #     case '/':
+            #         self.calc.division(self.get_arg('Введите 2-й аргумент: '))
+            #     case '=':
+            #         print(self.calc.get_result())
+            #         self.ask_user()
+            #     case _:
+            #         print('Неизвестная команда')
+            # конструкция if-elif-else
+            if cmd == '+':
+                self.calc.addition(self.get_arg('Введите 2-й аргумент: '))
+            elif cmd == '-':
+                self.calc.subtraction(
+                    self.get_arg('Введите 2-й аргумент: '))
+            elif cmd == '*':
+                self.calc.multiplication(
+                    self.get_arg('Введите 2-й аргумент: '))
+            elif cmd == '/':
+                self.calc.division(self.get_arg('Введите 2-й аргумент: '))
+            elif cmd == '=':
+                print(self.calc.get_result())
+                self.ask_user()
+            else:
+                print('Неизвестная команда')
 
     @staticmethod
     def get_arg(text: str) -> N:
